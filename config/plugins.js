110321531a3dc34f7d589c754556e10d451e9f52
addonion <addonion@gmail.com>
2022-11-24 15:43:07 +0600

    test

--- config/plugins.js
+++ config/plugins.js
@@ -30,6 +30,6 @@ module.exports = ({ env }) => ({
   },
   ckeditor: {
     enabled: true,
-    resolve: "../src/plugins/strapi-plugin-ckeditor",
+    resolve: "./src/plugins/strapi-plugin-ckeditor",
   },
 });
