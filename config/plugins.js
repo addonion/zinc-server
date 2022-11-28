5a369e08fec2250551285f3ccfc8d6e192eadd62
addonion <addonion@gmail.com>
2022-11-28 09:31:27 +0600

    Vercel plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,13 @@
 module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: env("VERCEL_DEPLOY_PLUGIN_HOOK"),
+      apiToken: env("VERCEL_DEPLOY_PLUGIN_API_TOKEN"),
+      appFilter: env("VERCEL_DEPLOY_PLUGIN_APP_FILTER"),
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
   seo: {
     enabled: true,
   },
